5bc3a9ecade10fbf78e81c213e41095569a8d7d1
paopao <1173381422@qq.com>
2020-04-14 15:57:34 +0800

    chore: 生成log

--- commitlint.config.js
+++ commitlint.config.js
@@ -13,14 +13,14 @@ module.exports = {
       2,
       'always',
       [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
+        'feat', // 发布新功能
+        'fix',  // bug 修复
+        'docs',  // 文档
+        'style',  // 代码格式
+        'refactor', // 代码重构
         'perf',
-        'test',
-        'chore',
+        'test',   // 增加测试
+        'chore',  // 构建过程或辅助工具的变动
         'revert',
         'WIP'
       ]
